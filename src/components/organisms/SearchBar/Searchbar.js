45aced594954808607539a0cc4bc14ecda192855
Konrad Piegza <piegzini.k@gmail.com>
2022-01-05 17:55:30 +0100

    Style and add logic to searchbar

--- src/components/organisms/SearchBar/Searchbar.js
+++ src/components/organisms/SearchBar/Searchbar.js
@@ -1,7 +1,21 @@
 import { Input } from '../../atoms/Input/Input';
-import { SearchBarWrapper, StatusInfo } from './SearchBar.styles';
+import { InputWrapper, ResultItem, SearchBarWrapper, SearchingResults, StatusInfo } from './SearchBar.styles';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const SearchBar = () => {
+  const [searchingPhrase, setSearchingPhrase] = useState('');
+  const [matchingStudents, setMatchingStudents] = useState([]);
+
+  useEffect(() => {
+    axios
+      .post('/students/search', {
+        info: searchingPhrase,
+      })
+      .then(({ data }) => setMatchingStudents(data.students))
+      .catch((e) => console.log(e));
+  }, [searchingPhrase]);
+
   return (
     <SearchBarWrapper>
       <StatusInfo>
@@ -10,7 +24,12 @@ export const SearchBar = () => {
           <strong>Teacher</strong>
         </p>
       </StatusInfo>
-      <Input />
+      <InputWrapper>
+        <Input value={searchingPhrase} onChange={(e) => setSearchingPhrase(e.target.value)} />
+        <SearchingResults isVisible={matchingStudents.length > 0 ? 'visible' : 'hidden'}>
+          {matchingStudents.length > 0 ? matchingStudents.map(({ name }) => <ResultItem>{name}</ResultItem>) : null}
+        </SearchingResults>
+      </InputWrapper>
     </SearchBarWrapper>
   );
 };
--- src/mock/handlers/index.js
+++ src/mock/handlers/index.js
@@ -14,4 +14,10 @@ export const handlers = [
     }
     return res(ctx.status(200), ctx.json({ students }));
   }),
+
+  rest.post('/students/search', (req, res, ctx) => {
+    const { info } = req.body;
+    const matchingStudents = info.split('').join('') !== '' ? students.filter((user) => user.name.toLowerCase().includes(info.toLowerCase())) : [];
+    return res(ctx.status(200), ctx.json({ students: matchingStudents }));
+  }),
 ];
